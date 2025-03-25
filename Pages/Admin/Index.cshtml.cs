1c4279c5104dd75ab6032139f3ce3e1414b2aae6
Hossein <h.younesian.farid@gmail.com>
2025-03-25 22:56:21 +0100

    Admin

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using NeoStore.Data;
 using NeoStore.Models;
+
 
 namespace NeoStore.Pages.Admin
 {
     public class IndexModel : PageModel
     {
-
-
-
         private EshopContext _context;
 
         public IndexModel(EshopContext context)
@@ -22,7 +21,7 @@ namespace NeoStore.Pages.Admin
         public IEnumerable<Product> Products { get; set; }
         public void OnGet()
         {
-            Products = _context.Products;
+            Products = _context.Products.Include(p=>p.Item);
         }
         public void OnPost()
         {
