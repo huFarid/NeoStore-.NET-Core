4450de5f0df1e5b1c3f4f64ca943a121c26b8985
Hossein Younesian Farid <h.younesian.farid@gmail.com>
2026-01-01 11:47:19 +0100

    Add form.

---
+++ Models/AddEditProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NeoStore.Models
+{
+    public class AddEditProductViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
--- Pages/Admin/Add.cshtml.cs
+++ Pages/Admin/Add.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NeoStore.Models;
 
 namespace NeoStore.Pages.Admin
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public AddEditProductViewModel Product { get; set; }
         public void OnGet()
         {
+
+
         }
     }
 }
