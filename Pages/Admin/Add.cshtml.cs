5e13ab363e0f49d462dd990f9d795aefa693f1be
Hossein Younesian Farid <h.younesian.farid@gmail.com>
2026-01-11 15:11:29 +0100

    AddNewProduct

--- Pages/Admin/Add.cshtml.cs
+++ Pages/Admin/Add.cshtml.cs
@@ -1,17 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NeoStore.Data;
 using NeoStore.Models;
+using System.Threading.Tasks;
 
 namespace NeoStore.Pages.Admin
 {
     public class AddModel : PageModel
     {
+
+        private readonly EshopContext _context;
+        public AddModel(EshopContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public AddEditProductViewModel Product { get; set; }
+        
         public void OnGet()
         {
 
 
         }
+
+       public void OnPost() { 
+        
+            if(!ModelState.IsValid)
+            {
+                return;
+            }
+
+            var newProduct = new Product()
+            {
+                Name = Product.Name,
+                Description = Product.Description,
+                Item = new Item()
+                {
+                    Price = Product.Price,
+                    QuantityInStock = Product.QuantityInStock
+                    
+                }
+            };
+
+
+
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+        }
+
+        
+
+      
     }
 }
