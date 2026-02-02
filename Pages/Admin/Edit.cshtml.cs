3f21d96f3f65cecf544ece7d10fddc4f290ca1bd
Hossein Younesian Farid <h.younesian.farid@gmail.com>
2026-02-02 23:48:25 +0100

    Edit.

--- Pages/Admin/Edit.cshtml.cs
+++ Pages/Admin/Edit.cshtml.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NeoStore.Data;
+using NeoStore.Models;
 
 namespace NeoStore.Pages.Admin
 {
     public class EditModel : PageModel
     {
+        public AddEditProductViewModel Product { get; set; }
+        EshopContext _context;
+        public EditModel(EshopContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
         }
